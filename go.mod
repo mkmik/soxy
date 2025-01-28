96208ff7bf8e1ce074d126aaf4a6a7a7ac541c80
Marko Mikulicic <mmikulicic@gmail.com>
2025-01-28 16:02:43 +0100

    CLI

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mkmik/soxy
 
 go 1.23.5
+
+require github.com/alecthomas/kong v1.6.1
--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"runtime/debug"
 
+	"github.com/alecthomas/kong"
+)
+
+// set by goreleaser
+var version = "(devel)"
+
+type Context struct {
+	*CLI
+}
+
+type CLI struct {
+	Version kong.VersionFlag `name:"version" help:"Print version information and quit"`
+}
+
+func (cmd *CLI) Run(cli *Context) error {
+	return nil
+}
+
+func getVersion() string {
+	if bi, ok := debug.ReadBuildInfo(); ok {
+		if v := bi.Main.Version; v != "" && v != "(devel)" {
+			return v
+		}
+	}
+	// otherwise fallback to the version set by goreleaser
+	return version
+}
+
+func main() {
+	var cli CLI
+	ctx := kong.Parse(&cli,
+		kong.Description(`Simple reverse proxy with CLI configuration.`),
+		kong.UsageOnError(),
+		kong.Vars{
+			"version": getVersion(),
+		},
+		kong.ConfigureHelp(kong.HelpOptions{
+			Compact: true,
+			Summary: true,
+		}),
+	)
+
+	err := ctx.Run(&Context{CLI: &cli})
+	ctx.FatalIfErrorf(err)
 }
